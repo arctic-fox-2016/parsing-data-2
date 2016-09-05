e1183ea442343c2fbdaa3ba3471d3ed55088ff97
Fadhli Ramadhani Arislan & Peter Raswono <pair+fadhlirams.praswono@hacktiv8.com>
2016-09-05 21:22:34 +0700

    Fadhli Peter

--- parser.js
+++ parser.js
@@ -1,34 +1,90 @@
 "use strict"
+var csv = require("basic-csv");
+var yaml = require('js-yaml')
+var jsonfile = require('jsonfile')
+var fs = require('fs');
+var list = []
+var file = "people.csv"
 
-class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
-}
+csv.readCSV("people.csv", {
+  dropHeader: true
+}, function (error, rows) {
 
-class PersonParser {
-
-  constructor(file) {
-    this.file = file
-    this.people = []
+  class Person {
+    constructor(rows) {
+      this._people = rows
+    }
   }
 
-  get people() {
-    // If we've already parsed the CSV file, don't parse it again.
-    if (this.people)
-      return this.people
+  class PersonParser {
+    constructor(file) {
+      this._file = file
+      this._people = rows
+      this._new_people = ""
+      this._new_people_count = 0
+    }
 
-    // We've never called people before, now parse the CSV file
-    // and return an Array of Person objects here.  Save the
-    // Array in the this.people instance variable.
+    get people() {
+      if (this._people)
+        return this._people
+    }
+
+    add_person(first_name, last_name, email, phone, created_at) {
+      let date = new Date(created_at)
+      this._new_people += (this._people.length + this._new_people_count + 1) + "," +  first_name + "," + last_name + "," + email + "," + phone + "," + date + "\n"
+      this._new_people_count++
+    }
+
+    save_as_yaml() {
+      try {
+        var doc = yaml.safeLoad(fs.readFileSync(this._file, 'utf8'))
+        let yaml_data = doc
+
+        fs.writeFile("people.yaml", yaml_data, function(err) {
+          if (err) throw 'error writing file: ' + err;
+        });
+
+        console.log(`Added data into people.yaml.`)
+        this._new_people_count = 0
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+    save_as_json() {
+      let str = ""
+      let json_data = {}
+      for (var i = 0; i < this._people.length; i++) {
+        // json_data.[i+1].id = i+1
+        // json_data.[i+1].first_name = this._people[i][1]
+        // json_data.[i+1].last_name = this._people[i][2]
+        // json_data.[i+1].email = this._people[i][3]
+        // json_data.[i+1].phone = this._people[i][4]
+        // json_data.[i+1].created_at = Date(this._people[i][5])
+
+        str += "{" +
+          "id: '" + this._people[i][0] + "'," +
+          "first_name: '" + this._people[i][1] + "'," +
+          "last_name: '" + this._people[i][2] + "'," +
+          "email: '" + this._people[i][3] + "'," +
+          "phone: '" + this._people[i][4] + "'," +
+          "created_at: '" + Date(this._people[i][5]) + "'}"
+      }
+
+      json_data = JSON.parse(JSON.stringify(str))
+
+      console.log(json_data)
+
+      var file = 'people.json'
+      jsonfile.writeFile(file, json_data, function (err) {
+        console.error(err)
+      })
+    }
   }
 
-  save() {}
+  // Initiate parser
+  let parser = new PersonParser(file)
+  // parser.save_as_yaml()
+  parser.save_as_json()
 
-  save_as_yaml() {}
-
-  save_as_json() {}
-}
-
-var parser = new PersonParser('people.csv')
-
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+});
